2178692b6f3b026f64c4c6ef27d370bf1e0762a0
BTNguyen9280 <briannguyen9280@yahoo.com>
2022-11-15 20:31:53 -0800

    add playround function

--- javascript.js
+++ javascript.js
@@ -11,4 +11,24 @@ const computerchoice = () =>{
     }
 }
 
-console.log(computerchoice())
+let userinput = prompt("Enter rock, paper, or scissors.")
+userinput.toLowerCase
+
+const playround = (userchoice,computerchoice) => {
+    if (userchoice == computerchoice) {
+        return `You both chose ${userchoice} It's a tie!`
+    }
+    else if (userchoice == `rock` && computerchoice == `paper`){
+    return `You win! rock beats paper.`
+    }
+    else if (userchoice == `paper` && computerchoice == `rock`) {
+        return `You win! paper beats rock.`
+    }
+    else if(userchoice==`scissors` && computerchoice == `paper`) {
+        return `You win! scissors beats paper`
+    }
+    else {
+        return `You lose! ${computerchoice} beats ${userchoice}`
+    }       
+}
+
